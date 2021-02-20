1b2d5ea996970887ac70af9b71d97e3fbaedecc2
James Swineson <github@public.swineson.me>
2021-02-20 20:28:07 +0800

    update comments

--- dns_server_soa.go
+++ dns_server_soa.go
@@ -8,15 +8,16 @@ import (
 func handleSOA(this *dnsRequestHandler, r, msg *dns.Msg) {
 	log.Printf("[DNS] SOA %s\n", msg.Question[0].Name)
 
+	// used to answer Windows Server's DNS server liveness probe
 	msg.Answer = append(msg.Answer, &dns.SOA{
-		Hdr: dns.RR_Header{Name: msg.Question[0].Name, Rrtype: r.Question[0].Qtype, Class: r.Question[0].Qclass, Ttl: DNSDefaultTTL},
-		Ns: "ns1.example.com.",
-		Mbox: "dnsmaster.example.com.",
-		Serial: 114514,
+		Hdr:     dns.RR_Header{Name: msg.Question[0].Name, Rrtype: r.Question[0].Qtype, Class: r.Question[0].Qclass, Ttl: DNSDefaultTTL},
+		Ns:      "ns1.example.com.",
+		Mbox:    "dnsmaster.example.com.",
+		Serial:  114514,
 		Refresh: 60,
-		Retry: 10,
-		Expire: 3600000,
-		Minttl: DNSDefaultTTL,
+		Retry:   10,
+		Expire:  3600000,
+		Minttl:  DNSDefaultTTL,
 	})
 	return
 }
