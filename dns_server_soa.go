7529da9483bcb57d40e39d2de2f66716b0eb0847
James Swineson <github@public.swineson.me>
2021-02-20 16:17:40 +0800

    fix grammar

--- dns_server_soa.go
+++ dns_server_soa.go
@@ -10,14 +10,13 @@ func handleSOA(this *dnsRequestHandler, r, msg *dns.Msg) {
 
 	msg.Answer = append(msg.Answer, &dns.SOA{
 		Hdr: dns.RR_Header{Name: msg.Question[0].Name, Rrtype: r.Question[0].Qtype, Class: r.Question[0].Qclass, Ttl: DNSDefaultTTL},
-		Ns = "ns1.example.com.",
-		Mbox = "dnsmaster.example.com.",
-		Serial = 114514,
-		Refresh = 60,
-		Retry = 10,
-		Expire = 3600000,
-		Minttl = DNSDefaultTTL,
+		Ns: "ns1.example.com.",
+		Mbox: "dnsmaster.example.com.",
+		Serial: 114514,
+		Refresh: 60,
+		Retry: 10,
+		Expire: 3600000,
+		Minttl: DNSDefaultTTL,
 	})
 	return
-	}
 }
