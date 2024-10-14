2d13b33d38f6712caca00baf9a8c6bdc70cc006b
Jamesits <osscontrib@ciexyz.net>
2024-10-14 15:35:15 +0800

    fix SOA reply when being added as a conditional forwarder in Windows DNS server

--- dns_server_soa.go
+++ dns_server_soa.go
@@ -2,41 +2,20 @@ package main
 
 import (
 	"github.com/miekg/dns"
-	"strings"
 )
 
-func handleSOA(this *dnsRequestHandler, r, msg *dns.Msg) {
-	switch strings.ToLower(msg.Question[0].Name) {
-	case ".":
-		// When adding an upstream in Windows Server's DNS server, a SOA question to `.` will be generated to probe if the upstream is alive
-		// Reply this hardcoded answer to pass the test
-		msg.Answer = append(msg.Answer, &dns.SOA{
-			Hdr:     dns.RR_Header{Name: msg.Question[0].Name, Rrtype: r.Question[0].Qtype, Class: r.Question[0].Qclass, Ttl: DNSDefaultTTL},
-			Ns:      "a.root-servers.net.",
-			Mbox:    "nstld.verisign-grs.com.",
-			Serial:  114514,
-			Refresh: 60,
-			Retry:   10,
-			Expire:  3600000,
-			Minttl:  DNSDefaultTTL,
-		})
-		return
-
-	case "dns.msftncsi.com.":
-		// in one unknown case, SOA record to dns.msftncsi.com is requested
-		msg.Answer = append(msg.Answer, &dns.SOA{
-			Hdr:     dns.RR_Header{Name: msg.Question[0].Name, Rrtype: r.Question[0].Qtype, Class: r.Question[0].Qclass, Ttl: DNSDefaultTTL},
-			Ns:      "ns1-205.azure-dns.com.",
-			Mbox:    "azuredns-hostmaster.microsoft.com.",
-			Serial:  1,
-			Refresh: 60,
-			Retry:   10,
-			Expire:  3600000,
-			Minttl:  DNSDefaultTTL,
-		})
-		return
-
-	default:
-		handleDefault(this, r, msg)
-	}
+func handleSOA(_ *dnsRequestHandler, r, msg *dns.Msg) {
+	// When adding an upstream in Windows Server's DNS server, a SOA question to `.` (or the specific domain, if adding
+	// as a conditional forwarder) will be generated to probe if the upstream is alive.
+	// Reply this hardcoded answer to pass the test.
+	msg.Answer = append(msg.Answer, &dns.SOA{
+		Hdr:     dns.RR_Header{Name: msg.Question[0].Name, Rrtype: r.Question[0].Qtype, Class: r.Question[0].Qclass, Ttl: DNSDefaultTTL},
+		Ns:      "a.root-servers.net.",
+		Mbox:    "nstld.verisign-grs.com.",
+		Serial:  114514,
+		Refresh: 60,
+		Retry:   10,
+		Expire:  3600000,
+		Minttl:  DNSDefaultTTL,
+	})
 }
